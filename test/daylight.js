90e8153ee93655cb3c83d8866e2dd158c10c34fe
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-05-06 00:33:13 +0900

    Add tests for conversions of hex code including alpha value

--- test/daylight.js
+++ test/daylight.js
@@ -66,6 +66,52 @@ describe("Daylight.getReflectionColor", () => {
         expect(actual).toBe("linear-gradient(#123, #456);");
     });
 
+    // hex4-1:
+    it("hex4-1: 第1引数が16進数(4桁)の色表現の場合は、調整した色の16進数(4桁)表現が返却される", () => {
+        // テストの準備
+        const daylight = new Daylight();
+        const expression = "#012f";
+        const config = {
+            now: new Date(2000, 0, 1, 12, 0, 0),
+            impact: 0.1,
+            theme: {
+                "11:00:00": "#345",
+                "13:00:00": "#bdf"
+            }
+        };
+        
+        // テスト対象の処理を実行
+        const actual = daylight.getReflectionColor(expression, config);
+        
+        // 結果を検証
+        expect(actual).toBe("#123f");
+        
+        // NOTE :
+        // 基本的な変換については hex3-1 を参照
+        // hex4 の場合は、アルファ値に該当する4桁目が結果に付与
+    });
+
+    // hex4-2:
+    it("hex4-2: 第1引数が16進数(4桁)の色表現を含む場合は、調整した色の16進数(4桁)表現に置換した内容が返却される", () => {
+        // テストの準備
+        const daylight = new Daylight();
+        const expression = "linear-gradient(#012e, #345f);";
+        const config = {
+            now: new Date(2000, 0, 1, 12, 0, 0),
+            impact: 0.1,
+            theme: {
+                "11:00:00": "#678",
+                "13:00:00": "#bdf"
+            }
+        };
+
+        // テスト対象の処理を実行
+        const actual = daylight.getReflectionColor(expression, config);
+
+        // 結果を検証
+        expect(actual).toBe("linear-gradient(#123e, #456f);");
+    });
+
     // hex6-1:
     it("hex6-1: 第1引数が16進数(6桁)の色表現の場合は、調整した色の16進数(6桁)表現が返却される", () => {
         // テストの準備
@@ -106,5 +152,47 @@ describe("Daylight.getReflectionColor", () => {
 
         // 結果を検証
         expect(actual).toBe("linear-gradient(#0f2032, #3c4e60);");
+    });
+
+    // hex8-1:
+    it("hex8-1: 第1引数が16進数(8桁)の色表現の場合は、調整した色の16進数(8桁)表現が返却される", () => {
+        // テストの準備
+        const daylight = new Daylight();
+        const expression = "001122ff";
+        const config = {
+            now: new Date(2000, 0, 1, 12, 0, 0),
+            impact: 0.1,
+            theme: {
+                "11:00:00": "#334455",
+                "13:00:00": "#bbddff"
+            }
+        };
+
+        // テスト対象の処理を実行
+        const actual = daylight.getReflectionColor(expression, config);
+
+        // 結果を検証
+        expect(actual).toBe("#0c1e30ff");
+    });
+
+    // hex8-2:
+    it("hex8-2: 第1引数が16進数(8桁)の色表現を含む場合は、調整した色の16進数(8桁)表現に置換した内容が返却される", () => {
+        // テストの準備
+        const daylight = new Daylight();
+        const expression = "linear-gradient(#001122ee, #334455ff);";
+        const config = {
+            now: new Date(2000, 0, 1, 12, 0, 0),
+            impact: 0.1,
+            theme: {
+                "11:00:00": "#667788",
+                "13:00:00": "#bbddff"
+            }
+        };
+
+        // テスト対象の処理を実行
+        const actual = daylight.getReflectionColor(expression, config);
+
+        // 結果を検証
+        expect(actual).toBe("linear-gradient(#0f2032ee, #3c4e60ff);");
     });
 });
