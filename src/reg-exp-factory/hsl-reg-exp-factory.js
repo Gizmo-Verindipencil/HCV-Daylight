7dd92222a2bb69e7868e9aef0abc95aebe74d1a6
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-06-25 13:13:06 +0900

    Add a factory for hsl regular expression

---
+++ src/reg-exp-factory/hsl-reg-exp-factory.js
@@ -0,0 +1,17 @@
+/**
+ * HSL表現に関する正規表現の作成処理を提供します。
+ */
+class HslRegExpFactory {
+    /**
+     * 正規表現を作成します。
+     * @returns {RegExp} 正規表現を返します。
+     */
+    create() {
+        const nonPercentValue = "\\s{0,}([1-2]){0,1}([0-9]){1,2}(\\.[0-9]{1,}){0,1}\\s{0,}";
+        const percentValue = "\\s{0,}[0-9]{1,}(\\.[0-9]{1,}){0,1}%{0,1}\\s{0,}";
+        const percentValues = [...Array(2)].map(x => percentValue);
+        return new RegExp(`^\\s{0,}hsl\\(${nonPercentValue},${percentValues.join(",")}\\)\\s{0,}$`, "i");
+    }
+}
+
+export { HslRegExpFactory };
---
+++ test/reg-exp-factory/hsl.js
@@ -0,0 +1,77 @@
+import { HslRegExpFactory } from "../../src/reg-exp-factory/hsl-reg-exp-factory.js";
+
+// hsl
+// HSL表現(例: hsl(0, 0%, 0%) )に関するテスト
+
+describe("HslRegExpFactory - hsl_", () => {
+    // hsl_1:
+    it("1: 値が整数", () => {
+        // テスト対象の正規表現を作成
+        const factory = new HslRegExpFactory();
+        const regExp = factory.create();
+
+        // 結果を検証
+        const values = [ "1", " 2", "3 " ];
+        const getPercent = x => x != x.trimEnd() ? `${x.trimEnd()}% ` : `${x}%`;
+        const expressions = values.map(x => `hsl(${x},${[...Array(2)].map(y => getPercent(x)).join(",")})`);
+        for (const expression of expressions) {
+            const result = regExp.test(expression);
+            expect(result).toBe(true);
+        }
+    });
+
+    // hsl_2:
+    it("2: 値が小数", () => {
+        // テスト対象の正規表現を作成
+        const factory = new HslRegExpFactory();
+        const regExp = factory.create();
+
+        // 結果を検証
+        const values = [ "0.1", " 0.2", "0.3 " ];
+        const getPercent = x => x != x.trimEnd() ? `${x.trimEnd()}% ` : `${x}%`;
+        const expressions = values.map(x => `hsl(${x},${[...Array(2)].map(y => getPercent(x)).join(",")})`);
+        for (const expression of expressions) {
+            const result = regExp.test(expression);
+            expect(result).toBe(true);
+        }
+    });
+
+    // hsl_3:
+    it("3: 前後に空白あり", () => {
+        // テスト対象の正規表現を作成
+        const factory = new HslRegExpFactory();
+        const regExp = factory.create();
+
+        // 結果を検証
+        for (const expression of [
+            " hsl(0,0%,0%) ",
+            " hsl(1,1%,1%)",
+            "hsl(2,2%,2%) "
+        ]) {
+            const result = regExp.test(expression);
+            expect(result).toBe(true);
+        }
+    });
+
+    // hsl_4:
+    it("4: その他アンマッチ", () => {
+        // テスト対象の正規表現を作成
+        const factory = new HslRegExpFactory();
+        const regExp = factory.create();
+
+        // 結果を検証
+        for (const expression of [
+            "abc",
+            "#000",
+            "#0000",
+            "#000000",
+            "#00000000",
+            "rgb(0%,0%,0%)",
+            "rgb(0,0,0,0)",
+            "rgb(0,0,0,0)"
+        ]) {
+            const result = regExp.test(expression);
+            expect(result).toBe(false);
+        }
+    });
+});
