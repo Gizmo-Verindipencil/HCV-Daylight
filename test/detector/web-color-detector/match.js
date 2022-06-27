3427124b2fb988a99dd1f51a45d1a37ec1d483f8
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-06-28 08:41:48 +0900

    Add a detector for web color expression

---
+++ test/detector/web-color-detector/match.js
@@ -0,0 +1,72 @@
+import { WebColorDetector } from "../../../src/detector/web-color-detector.js";
+import { ExpressionSetFactory } from "../expression-set-factory.js";
+import { WebColorNameList } from "./web-color-name-list.js";
+
+// web-color
+// WEBカラー表現(例: red )に関するテスト
+
+describe("WebColorDetector.match - ", () => {
+    // 1:
+    it("1: 大文字", () => {
+        // テスト対象の正規表現を作成
+        const detector = new WebColorDetector();
+
+        // 結果を検証
+        const expressions = WebColorNameList.getNames().map(x => x.toUpperCase());
+        for (const expression of expressions) {
+            const result = detector.match(expression);
+            expect(result).toEqual(true);
+        }
+    });
+
+    // 2:
+    it("2: 小文字", () => {
+        // テスト対象の正規表現を作成
+        const detector = new WebColorDetector();
+
+        // 結果を検証
+        const expressions = WebColorNameList.getNames().map(x => x.toLowerCase());
+        for (const expression of expressions) {
+            const result = detector.match(expression);
+            expect(result).toEqual(true);
+        }
+    });
+
+    // 3:
+    it("3: 前後マージン", () => {
+        // テスト対象の正規表現を作成
+        const detector = new WebColorDetector();
+
+        // 結果を検証
+        const i = 0;
+        const addMargins = x => {
+            const result = i === 0 ? x :
+                i === 1 ? ` ${x}` :
+                i === 2 ? `${x} ` :
+                "";
+            i = i === 2 ? 0 : i + 1;
+            return result;
+        }
+        const expressions = WebColorNameList.getNames().map(x => addMargins(x));
+        for (const expression of expressions) {
+            const result = detector.match(expression);
+            expect(result).toEqual(true);
+        }
+    });
+
+    // 4:
+    it("4: その他", () => {
+        // テスト対象の正規表現を作成
+        const detector = new WebColorDetector();
+
+        // 結果を検証
+        const expressions = [].concat(
+            WebColorNameList.getNames().map(x => x + "x"),
+            Object.values(ExpressionSetFactory.create()).flat()
+        );
+        for (const expression of expressions) {
+            const result = detector.match(expression);
+            expect(result).toEqual(false);
+        }
+    });
+});
