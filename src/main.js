62a90b1eb3c355765ad4950f366a644da2d20b93
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2022-10-10 12:36:35 +0900

    Replace main.js with index.js (index.js is more common)

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { Daylight } from "./daylight.js";
-
-function getReflectionColor(expression, config) { return Daylight.getReflectionColor(expression, config); }
-function reflectToElement (element, property, config) { Daylight.reflectToElement(element, property, config); }
-function reflectToPage(property, config) { Daylight.reflectToPage(property, config); }
-
-export { getReflectionColor };
-export { reflectToElement };
-export { reflectToPage };
